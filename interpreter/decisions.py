26a50904bdc2f3856fe3e2b6b621f9695020edc3
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-07-28 09:32:35 -0700

    remove evaluate()

--- interpreter/decisions.py
+++ interpreter/decisions.py
@@ -23,20 +23,3 @@ class BinaryOperationNode:
 
     def __repr__(self):
         return f"({self.left_node}, {self.operation}, {self.right_node})"
-
-
-def evaluate(node: BinaryOperationNode | NumberNode):
-    if isinstance(node, NumberNode):
-        return node.value
-
-    left = evaluate(node.left_node)
-    right = evaluate(node.right_node)
-
-    if node.operation.type == TokenType.PLUS:
-        return left + right
-    elif node.operation.type == TokenType.MINUS:
-        return left - right
-    elif node.operation.type == TokenType.MUL:
-        return left * right
-    elif node.operation.type == TokenType.DIV:
-        return left / right
